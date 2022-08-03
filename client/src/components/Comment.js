cdd64a8404149ae3241658d5186eb7b854d7d2b4
vollmetal <sarah@avins.org>
2022-08-03 13:30:17 -0400

    Can create and view posts Need to make UI to make comments

---
+++ client/src/components/Comment.js
@@ -0,0 +1,12 @@
+import { Alert, Box, Button, Card, CardContent, List, TextField, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink, useLocation } from "react-router-dom";
+import { API_URL, POST_URL } from "../env";
+import * as lightStyles from "../Styles/lightStyle";
+
+function Comment () {
+    
+}
+
+export default Comment
